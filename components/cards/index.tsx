6fbc0c66880e5da24f7fdc7e27b515c279ef2dab
MrJadaml <spadam77@gmail.com>
2021-11-28 09:34:44 -0800

    basic doc fetching

--- components/cards/index.tsx
+++ components/cards/index.tsx
@@ -1,15 +1,38 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { Card } from '../card'
 
 interface CardsTypes {
   cards?: Array<any>
 }
 
-export const Cards: FC<CardsTypes> = ({
-  cards,
-}) => {
+export const Cards: FC<CardsTypes> = ({ }) => {
+  const [cards, setCards] = useState([])
+  const [cardDocs, cardDocsLoading, cardDocsError] = useCollection(
+    firebase.firestore().collection('cards'),
+    {},
+  )
+
+  useEffect(() => {
+    if (!cardDocsLoading && cards) {
+      const cards = cardDocs.docs.map(doc => doc.data())
+      setCards(cards)
+    }
+  }, [cardDocs])
+
+
   return (
     <div>
-      cards....
+      {cards.map(({ title, description, flavorText }) => (
+        <Card
+          title={title}
+          description={description}
+          flavorText={flavorText}
+        />
+      ))}
     </div>
-  );
+  )
 }
